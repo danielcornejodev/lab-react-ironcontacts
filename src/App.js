8e9a19820861a28dbea19453d295177240fa94c0
Daniel Cornejo <contact@danielcornejo.dev>
2023-07-03 13:34:05 -0400

    Iteration 2 complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import contactsData from "./contacts.json";
+import { useState } from "react";
+
 import './App.css';
 
 function App() {
+
+  const [contacts, setContacts] = useState(contactsData.slice(0, 5));
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Contacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            const displayOscar = function() {
+              let oscarRatingResults = '';
+
+              if (contact.wonOscar) {
+                oscarRatingResults = '🏆';
+              } else {
+                oscarRatingResults = '';
+              }
+              return oscarRatingResults;
+            };
+            
+            const displayEmmy = function() {
+              let emmyRatingResults = '';
+
+              if (contact.wonEmmy) {
+                emmyRatingResults = '🏆';
+              } else {
+                emmyRatingResults = '';
+              }
+              return emmyRatingResults;
+            };
+
+            
+            return (
+              <tr key={contact.id}>
+                <td>
+                  <img src={contact.pictureUrl} alt={contact.name} height="100" />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                <td>{displayOscar()}</td>
+                <td>{displayEmmy()}</td>
+              </tr>
+            )
+
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
